1ff9540c293712f593741759f2310f75a8544844
Quan Pham <doraemonfanclub@gmail.com>
2018-12-20 13:55:07 +0700

    Added Stylus loader

---
+++ webpack.dev.js
@@ -0,0 +1,85 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 9000,
+    hot: true
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[name]_[local]_[hash:base64:10]',
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+              javascriptEnabled: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(styl)$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true
+            }
+          },
+          {
+            loader: 'stylus-loader'
+          }
+        ],
+        fallback: 'style-loader'
+      }
+    ]
+  }
+})
